e05b4e84ac1da85261bbe7790946e90a22549803
klear1990 <klear1990coding@gmail.com>
2021-03-27 03:28:25 -0700

    Homework_20210123_all

---
+++ Homework/20210123/abc143_a.py
@@ -0,0 +1,9 @@
+H = input().split()
+ 
+a= int(H[0])
+b= int(H[1])
+ 
+if 2*b >= a:
+    print(0)
+else:
+    print(a-(b*2))
---
+++ Homework/20210123/abc144_a.py
@@ -0,0 +1,10 @@
+H = input().split()
+ 
+a= int(H[0])
+b= int(H[1])
+ 
+if a < 10 and b <10:
+    print(a*b)
+else:
+    print(-1)
+    
